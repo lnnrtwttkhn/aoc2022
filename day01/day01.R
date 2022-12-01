83f65c0bcb28d3c01e194b214b4accd493c0ee73
Lennart Wittkuhn <wittkuhn@mpib-berlin.mpg.de>
2022-12-01 10:03:22 +0100

    add solution for part two and add example data

--- day01/day01.R
+++ day01/day01.R
@@ -2,8 +2,17 @@ if (!requireNamespace("pacman")) install.packages("pacman")
 packages_cran = c("here")
 pacman::p_load(char = packages_cran)
 data <- readLines(here::here("day01", "data.txt"))
-# credit: https://stackoverflow.com/a/71130251
-i1 <- !nzchar(data)
-data_split <- unname(split(data[!i1], cumsum(i1)[!i1]))
-data_sum <- lapply(data_split, function(x) sum(as.integer(x)))
-solution <- max(unlist(data_sum))
+example <- readLines(here::here("day01", "example.txt"))
+# part one
+get_sums = function(x) {
+  # credit: https://stackoverflow.com/a/71130251
+  i1 <- !nzchar(x)
+  x_split <- unname(split(x[!i1], cumsum(i1)[!i1]))
+  x_sums <- unlist(lapply(x_split, function(x) sum(as.integer(x))))
+  return(x_sums)
+}
+solution <- max(get_sums(data))
+example_solution <- max(get_sums(example))
+# part two
+sums <- get_sums(data)
+solution <- sum(sort(sums)[seq(length(sums) - 2, length(sums))])
